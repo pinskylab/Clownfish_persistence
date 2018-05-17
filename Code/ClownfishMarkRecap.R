4637a0174cac843abea4a30375d2b388860ebfac
Allison Dedrick <agdedrick@ucdavis.edu>
2018-05-17 11:22:49 -0400

    Recap MARK work in progress, saving encounter history data frame with size, tail color, site info for use in determining distance from track to their anemones each sampling season.

--- Code/ClownfishMarkRecap.R
+++ Code/ClownfishMarkRecap.R
@@ -51,190 +51,202 @@ CreateEncounterSummary <- function(start.year, end.year, tagged.fish) {
   return(encounters.out)
 }
 
-# Function to calculate likelihood for 3 years of sampling (check this!!) with fish tagged in year 2 included too
-Like3SampleYears4vars <- function(n111, n110, n101, n100, n011, n010, s1, s2, p2, p3) {
-  #like <- ((s1*p2*s2*p3)^n111)*((s1*p2*(1-s2*p3))^n110)*((s1*(1-p2)*s2*p3)^n101)*((1-s1*p2-s1*(1-p2)*s2*p3)^n100)
-  x2 <- 1 - s2 + s2*(1 - p3) #probability of not being seen after the second sampling session
-  x1 <- 1 - s1 + s1*(1-p2)*x2 #probability of not being seen after the first sampling session
-  
-  p111 <- s1*p2*s2*p3 #probability of encounter history 111
-  p110 <- s1*p2*x2 #probability of encounter history 110
-  p101 <- s1*(1 - p2)*s2*p3 #probability of encounter history 101
-  p100 <- x1 #probability of encounter history 100
-  p011 <- s2*p3 #probability of encounter history 011
-  p010 <- x2 #probability of encounter history 010
-  
-  like <- n111*log(p111) + n110*log(p110) + n101*log(p101) + n100*log(p100) + n011*log(p011) + n010*log(p010)
-  return(like)
-}
-
-# Same as above but for only two variables (assuming that survival and recapture probs are same through time)
-Like3SampleYears2vars <- function(n111, n110, n101, n100, n011, n010, s, p) {
-  #like <- ((s1*p2*s2*p3)^n111)*((s1*p2*(1-s2*p3))^n110)*((s1*(1-p2)*s2*p3)^n101)*((1-s1*p2-s1*(1-p2)*s2*p3)^n100)
-  x2 <- 1 - s + s*(1 - p) #probability of not being seen after the second sampling session
-  x1 <- 1 - s + s*(1-p)*x2 #probability of not being seen after the first sampling session
-  
-  p111 <- s*p*s*p #probability of encounter history 111
-  p110 <- s*p*x2 #probability of encounter history 110
-  p101 <- s*(1 - p)*s*p #probability of encounter history 101
-  p100 <- x1 #probability of encounter history 100
-  p011 <- s*p #probability of encounter history 011
-  p010 <- x2 #probability of encounter history 010
-  
-  like <- n111*log(p111) + n110*log(p110) + n101*log(p101) + n100*log(p100) + n011*log(p011) + n010*log(p010)
-  return(like)
-}
-
-# Just cycles through vectors of possible survival and recapture probabilities and calculates the likelihood
-ParamRangeCheck3SY2P_dfoutput <- function(s_vec, p_vec, nvals) { 
-  #set index values for various encouter histories (this is the way R orders them using table)
-  N010 <- 2
-  N011 <- 3
-  N100 <- 4
-  N101 <- 5
-  N110 <- 6
-  N111 <- 7
-  
-  #get numbers of each type of encounter history from the inputs
-  n111 <- nvals$Freq[N111]
-  n110 <- nvals$Freq[N110]
-  n101 <- nvals$Freq[N101]
-  n100 <- nvals$Freq[N100]
-  n011 <- nvals$Freq[N011]
-  n010 <- nvals$Freq[N010]
-  
-  survival <- data.frame(survival=rep(s_vec,length(p_vec)))
-  out <- survival
-  capture_list <- rep(p_vec[1], length(s_vec))
-  for (i in 2:length(p_vec)) {
-    cap_toadd <- rep(p_vec[i], length(s_vec))
-    capture_list <- c(capture_list, cap_toadd)
-  }
-  out$capture <- capture_list
-  like = NULL
-  
-  for (i in 1:dim(out)[1]) {
-    like1 <- Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, out$survival[i], out$capture[i])
-    like <- c(like, like1)
-  }
-  out$like=like
-  return(out)
-}
-
-# Gradient descent for 3 sample years, estimating 4 parameters (doesn't really seem to work, oscillates between all params essentially at 0 or all at 1)
-GradientDescent3SY4Params <- function(nvals, params0, nsteps, eps, del, highVal, lowVal) {
-  param1 <- rep(NA,1,nsteps)
-  out <- as.data.frame(param1)
-  out$param2 <- rep(NA,1,nsteps)
-  out$param3 <- rep(NA,1,nsteps)
-  out$param4 <- rep(NA,1,nsteps)
-  out$like <- rep(NA,1,nsteps)
-  
-  params <- params0
-  
-  #set index values for various encouter histories (this is the way R orders them using table) (001 is 1)
-  N010 <- 2
-  N011 <- 3
-  N100 <- 4
-  N101 <- 5
-  N110 <- 6
-  N111 <- 7
-  
-  #get numbers of each type of encounter history from the inputs
-  n111 <- nvals$Freq[N111]
-  n110 <- nvals$Freq[N110]
-  n101 <- nvals$Freq[N101]
-  n100 <- nvals$Freq[N100]
-  n011 <- nvals$Freq[N011]
-  n010 <- nvals$Freq[N010]
-  
-  for (i in 1:nsteps) { #doesn't work with Palanas data b /c get NANs
-    #store parameter values and likelihood
-    out$param1[i] <- params[1]
-    out$param2[i] <- params[2]
-    out$param3[i] <- params[3]
-    out$param4[i] <- params[4]
-    out$like[i] <- Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4])
-    
-    #calculate the gradient and update the parameters
-    df <- rep(NA, length(params))
-    df[1] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1]+eps, params[2], params[3], params[4]) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
-    df[2] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2]+eps, params[3], params[4]) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
-    df[3] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3]+eps, params[4]) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
-    df[4] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]+eps) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
-    
-    params <- params+del*df
-    
-    #if parameters have become <= 0 or >= 1, set to low val or high val
-    for (i in 1:length(params)) {
-      if (params[i] >= 1) {
-        params[i] <- highVal
-      }
-      if (params[i] <= 0) {
-        params[i] <- lowVal
-      }
-    }
-    #print(Like3SampleYears(n111, n110, n101, n100, params[1], params[2], params[3], params[4]))
-  }
-  return(out)
-}
-
-# Gradient Descent 3 years, 2 params
-GradientDescent3SY2Params <- function(nvals, params0, nsteps, eps, del, highVal, lowVal) {
-  survival_p <- rep(NA,1,nsteps)
-  out <- as.data.frame(survival_p)
-  out$recapture_p <- rep(NA,1,nsteps)
-  #out$param3 <- rep(NA,1,nsteps)
-  #out$param4 <- rep(NA,1,nsteps)
-  out$like <- rep(NA,1,nsteps)
-  
-  params <- params0
-  
-  #set index values for various encouter histories (this is the way R orders them using table) (001 is 1)
-  N010 <- 2
-  N011 <- 3
-  N100 <- 4
-  N101 <- 5
-  N110 <- 6
-  N111 <- 7
-  
-  #get numbers of each type of encounter history from the inputs
-  n111 <- nvals$Freq[N111]
-  n110 <- nvals$Freq[N110]
-  n101 <- nvals$Freq[N101]
-  n100 <- nvals$Freq[N100]
-  n011 <- nvals$Freq[N011]
-  n010 <- nvals$Freq[N010]
-  
-  for (i in 1:nsteps) { #doesn't work with Palanas data b /c get NANs
-    #store parameter values and likelihood
-    out$survival_p[i] <- params[1]
-    out$recapture_p[i] <- params[2]
-    out$like[i] <- Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2])
-    
-    #calculate the gradient and update the parameters 
-    df <- rep(NA, length(params))
-    df[1] <- (Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1]+eps, params[2]) - Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2]))/eps
-    df[2] <- (Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2]+eps) - Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2]))/eps
-    
-    params <- params+del*df
-    
-    #if parameters have become <= 0 or >= 1, set to low val or high val
-    for (i in 1:length(params)) {
-      if (params[i] >= 1) {
-        params[i] <- highVal
-      }
-      if (params[i] <= 0) {
-        params[i] <- lowVal
-      }
-    }
-  }
-  return(out)
-}
+# # Function to calculate likelihood for 3 years of sampling (check this!!) with fish tagged in year 2 included too
+# Like3SampleYears4vars <- function(n111, n110, n101, n100, n011, n010, s1, s2, p2, p3) {
+#   #like <- ((s1*p2*s2*p3)^n111)*((s1*p2*(1-s2*p3))^n110)*((s1*(1-p2)*s2*p3)^n101)*((1-s1*p2-s1*(1-p2)*s2*p3)^n100)
+#   x2 <- 1 - s2 + s2*(1 - p3) #probability of not being seen after the second sampling session
+#   x1 <- 1 - s1 + s1*(1-p2)*x2 #probability of not being seen after the first sampling session
+#   
+#   p111 <- s1*p2*s2*p3 #probability of encounter history 111
+#   p110 <- s1*p2*x2 #probability of encounter history 110
+#   p101 <- s1*(1 - p2)*s2*p3 #probability of encounter history 101
+#   p100 <- x1 #probability of encounter history 100
+#   p011 <- s2*p3 #probability of encounter history 011
+#   p010 <- x2 #probability of encounter history 010
+#   
+#   like <- n111*log(p111) + n110*log(p110) + n101*log(p101) + n100*log(p100) + n011*log(p011) + n010*log(p010)
+#   return(like)
+# }
+# 
+# # Same as above but for only two variables (assuming that survival and recapture probs are same through time)
+# Like3SampleYears2vars <- function(n111, n110, n101, n100, n011, n010, s, p) {
+#   #like <- ((s1*p2*s2*p3)^n111)*((s1*p2*(1-s2*p3))^n110)*((s1*(1-p2)*s2*p3)^n101)*((1-s1*p2-s1*(1-p2)*s2*p3)^n100)
+#   x2 <- 1 - s + s*(1 - p) #probability of not being seen after the second sampling session
+#   x1 <- 1 - s + s*(1-p)*x2 #probability of not being seen after the first sampling session
+#   
+#   p111 <- s*p*s*p #probability of encounter history 111
+#   p110 <- s*p*x2 #probability of encounter history 110
+#   p101 <- s*(1 - p)*s*p #probability of encounter history 101
+#   p100 <- x1 #probability of encounter history 100
+#   p011 <- s*p #probability of encounter history 011
+#   p010 <- x2 #probability of encounter history 010
+#   
+#   like <- n111*log(p111) + n110*log(p110) + n101*log(p101) + n100*log(p100) + n011*log(p011) + n010*log(p010)
+#   return(like)
+# }
+# 
+# # Just cycles through vectors of possible survival and recapture probabilities and calculates the likelihood
+# ParamRangeCheck3SY2P_dfoutput <- function(s_vec, p_vec, nvals) { 
+#   #set index values for various encouter histories (this is the way R orders them using table)
+#   N010 <- 2
+#   N011 <- 3
+#   N100 <- 4
+#   N101 <- 5
+#   N110 <- 6
+#   N111 <- 7
+#   
+#   #get numbers of each type of encounter history from the inputs
+#   n111 <- nvals$Freq[N111]
+#   n110 <- nvals$Freq[N110]
+#   n101 <- nvals$Freq[N101]
+#   n100 <- nvals$Freq[N100]
+#   n011 <- nvals$Freq[N011]
+#   n010 <- nvals$Freq[N010]
+#   
+#   survival <- data.frame(survival=rep(s_vec,length(p_vec)))
+#   out <- survival
+#   capture_list <- rep(p_vec[1], length(s_vec))
+#   for (i in 2:length(p_vec)) {
+#     cap_toadd <- rep(p_vec[i], length(s_vec))
+#     capture_list <- c(capture_list, cap_toadd)
+#   }
+#   out$capture <- capture_list
+#   like = NULL
+#   
+#   for (i in 1:dim(out)[1]) {
+#     like1 <- Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, out$survival[i], out$capture[i])
+#     like <- c(like, like1)
+#   }
+#   out$like=like
+#   return(out)
+# }
+# 
+# # Gradient descent for 3 sample years, estimating 4 parameters (doesn't really seem to work, oscillates between all params essentially at 0 or all at 1)
+# GradientDescent3SY4Params <- function(nvals, params0, nsteps, eps, del, highVal, lowVal) {
+#   param1 <- rep(NA,1,nsteps)
+#   out <- as.data.frame(param1)
+#   out$param2 <- rep(NA,1,nsteps)
+#   out$param3 <- rep(NA,1,nsteps)
+#   out$param4 <- rep(NA,1,nsteps)
+#   out$like <- rep(NA,1,nsteps)
+#   
+#   params <- params0
+#   
+#   #set index values for various encouter histories (this is the way R orders them using table) (001 is 1)
+#   N010 <- 2
+#   N011 <- 3
+#   N100 <- 4
+#   N101 <- 5
+#   N110 <- 6
+#   N111 <- 7
+#   
+#   #get numbers of each type of encounter history from the inputs
+#   n111 <- nvals$Freq[N111]
+#   n110 <- nvals$Freq[N110]
+#   n101 <- nvals$Freq[N101]
+#   n100 <- nvals$Freq[N100]
+#   n011 <- nvals$Freq[N011]
+#   n010 <- nvals$Freq[N010]
+#   
+#   for (i in 1:nsteps) { #doesn't work with Palanas data b /c get NANs
+#     #store parameter values and likelihood
+#     out$param1[i] <- params[1]
+#     out$param2[i] <- params[2]
+#     out$param3[i] <- params[3]
+#     out$param4[i] <- params[4]
+#     out$like[i] <- Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4])
+#     
+#     #calculate the gradient and update the parameters
+#     df <- rep(NA, length(params))
+#     df[1] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1]+eps, params[2], params[3], params[4]) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
+#     df[2] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2]+eps, params[3], params[4]) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
+#     df[3] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3]+eps, params[4]) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
+#     df[4] <- (Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]+eps) - Like3SampleYears4vars(n111, n110, n101, n100, n011, n010, params[1], params[2], params[3], params[4]))/eps
+#     
+#     params <- params+del*df
+#     
+#     #if parameters have become <= 0 or >= 1, set to low val or high val
+#     for (i in 1:length(params)) {
+#       if (params[i] >= 1) {
+#         params[i] <- highVal
+#       }
+#       if (params[i] <= 0) {
+#         params[i] <- lowVal
+#       }
+#     }
+#     #print(Like3SampleYears(n111, n110, n101, n100, params[1], params[2], params[3], params[4]))
+#   }
+#   return(out)
+# }
+# 
+# # Gradient Descent 3 years, 2 params
+# GradientDescent3SY2Params <- function(nvals, params0, nsteps, eps, del, highVal, lowVal) {
+#   survival_p <- rep(NA,1,nsteps)
+#   out <- as.data.frame(survival_p)
+#   out$recapture_p <- rep(NA,1,nsteps)
+#   #out$param3 <- rep(NA,1,nsteps)
+#   #out$param4 <- rep(NA,1,nsteps)
+#   out$like <- rep(NA,1,nsteps)
+#   
+#   params <- params0
+#   
+#   #set index values for various encouter histories (this is the way R orders them using table) (001 is 1)
+#   N010 <- 2
+#   N011 <- 3
+#   N100 <- 4
+#   N101 <- 5
+#   N110 <- 6
+#   N111 <- 7
+#   
+#   #get numbers of each type of encounter history from the inputs
+#   n111 <- nvals$Freq[N111]
+#   n110 <- nvals$Freq[N110]
+#   n101 <- nvals$Freq[N101]
+#   n100 <- nvals$Freq[N100]
+#   n011 <- nvals$Freq[N011]
+#   n010 <- nvals$Freq[N010]
+#   
+#   for (i in 1:nsteps) { #doesn't work with Palanas data b /c get NANs
+#     #store parameter values and likelihood
+#     out$survival_p[i] <- params[1]
+#     out$recapture_p[i] <- params[2]
+#     out$like[i] <- Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2])
+#     
+#     #calculate the gradient and update the parameters 
+#     df <- rep(NA, length(params))
+#     df[1] <- (Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1]+eps, params[2]) - Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2]))/eps
+#     df[2] <- (Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2]+eps) - Like3SampleYears2vars(n111, n110, n101, n100, n011, n010, params[1], params[2]))/eps
+#     
+#     params <- params+del*df
+#     
+#     #if parameters have become <= 0 or >= 1, set to low val or high val
+#     for (i in 1:length(params)) {
+#       if (params[i] >= 1) {
+#         params[i] <- highVal
+#       }
+#       if (params[i] <= 0) {
+#         params[i] <- lowVal
+#       }
+#     }
+#   }
+#   return(out)
+# }
 
 
 #################### Running things: ####################
 leyte <- read_db("Leyte")
+
+# fish <- leyte %>%
+#   tbl("clownfish")
+# 
+# anem <- leyte %>%
+#   tbl("anemones")
+# 
+# dives <- leyte %>%
+#   tbl("diveinfo")
+# save(fish, file='fish_db_05-07-2018')
+# save(anem, file='anem_db_05-07-2018')
+# save(dives, file='dives_db_05-07-2018')
 
 # # Pull out all the tagged fish (here, using saved database tables, not leyte)
 # allfish_fish <- leyte %>% 
@@ -370,8 +382,46 @@ encounters_all <- left_join(encounters_all, size_2016, by="tag_id")
 encounters_all <- left_join(encounters_all, size_2017, by="tag_id")
 encounters_all <- left_join(encounters_all, size_2018, by="tag_id")
 
+save(encounters_all, file=here("Data", "encounters_all.RData"))
+
 ##### Now, try running MARK!
-tagged.site <- mark(data=encounters_all, model = "CJS")
+# specifying site as group
+# make process and design data
+tagged.site.process = process.data(encounters_all, model="CJS", begin.time=2015, groups="site")
+tagged.site.ddl = make.design.data(tagged.site.process)
+
+# set different relationships
+Phi.dot = list(formula=~1)
+Phi.time = list(formula=~time)
+Phi.site = list(formula=~site)
+Phi.siteplustime = list(formua=~site+time)
+
+p.dot = list(formula=~1)
+p.time = list(formula=~time)
+p.site = list(formula=~site)
+p.siteplustime = list(formua=~site+time) # does this still pool sites at some level, assuming they come from the same dist and are related (like would in rethinking) or just break them out completely separately?
+
+# run some models
+tagged.site.phi.dot.p.dot = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.dot, p=p.dot))
+tagged.site.phi.time.p.dot = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.time, p=p.dot))
+tagged.site.phi.dot.p.time = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.dot, p=p.time))
+tagged.site.phi.time.p.time = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.time, p=p.time))
+tagged.site.phi.site.p.site = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.site, p=p.site))
+tagged.site.phi.time.p.siteplustime = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.site, p=p.siteplustime))
+
+#these don't run... must have something wrong? 
+tagged.site.phi.siteplustime.p.time = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.siteplustime, p=p.time))
+tagged.site.phi.siteplustime.p.siteplustime = mark(tagged.site.process, tagged.site.ddl, model.parameters=list(Phi=Phi.siteplustime, p=p.siteplustime))
+
+
+
+
+#tagged.site <- mark(data=encounters_all, model = "CJS")
+
+
+
+
+
 
 ##### Previous work using own likelihood functions...
 # Now, create encounter histories for fish at all sites combined
