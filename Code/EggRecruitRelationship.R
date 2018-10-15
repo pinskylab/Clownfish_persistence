71f4791b7768eb14e30d648d719d792e4e1381e5
Allison Dedrick <agdedrick@ucdavis.edu>
2018-10-15 17:08:18 -0400

    Clarified year vs egg year in joins and saved data outputs

--- Code/EggRecruitRelationship.R
+++ Code/EggRecruitRelationship.R
@@ -1,16 +1,16 @@
 # Look at egg-recruit relationship (in attempts to estimate survival from eggs to recruits)
 
 #################### Set-up: ####################
-##### Load relevant libraries
-library(RCurl) #allows running R scripts from GitHub
-library(RMySQL) #might need to load this to connect to the database?
-library(dplyr)
-library(tidyr)
-library(lubridate)
-library(stringr)
-library(ggplot2)
-library(RColorBrewer)
-library(here)
+# ##### Load relevant libraries
+# library(RCurl) #allows running R scripts from GitHub
+# library(RMySQL) #might need to load this to connect to the database?
+# library(dplyr)
+# library(tidyr)
+# library(lubridate)
+# library(stringr)
+# library(ggplot2)
+# library(RColorBrewer)
+# library(here)
 
 ##### Set constants (consider moving some of these to common constants and function script?)
 min_size_breedingF <- 6.0 # minimum size for filtering out breeding females
@@ -24,7 +24,10 @@ max_size_recruit <- 6.0 #maximum size to be considered a recruit from an egg pro
 sample_months <- c(3,4,5,6,7,8) #set months to avoid winter 2015 anem surveys
 dive_list <- c("0","C","D","E")
 
-##### Load other files
+##### Run and load other files
+# Source constants file
+source(file=here::here('Code','Constants_database_common_functions.R'))
+
 # load file with proportion habitat sampled estimates (from TotalAnemsAtSite.R)
 load(file=here("Data",'anem_sampling_table.RData')) #file with anems, after 2018 anems matched
 
@@ -54,71 +57,72 @@ prob_r <- c(0.5555556, 0.2647059, 0.8888889, 0.6666667, 0.2000000, #2016 recaptu
             0.3333333, 0.5789474, 0.6250000, 0.4090909) #2018 recapture dives
 
 #################### Functions: ####################
-# Functions and constants from my GitHub function/constant collection
-# script <- getURL("https://raw.githubusercontent.com/pinskylab/Clownfish_data_analysis/master/Code/Common_constants_and_functions.R?token=AH_ZQAXExRItr2tnepmkRr7NRt4hylZrks5aciBtwA%3D%3D", ssl.verifypeer = FALSE)
+# # Functions and constants from my GitHub function/constant collection
+# # script <- getURL("https://raw.githubusercontent.com/pinskylab/Clownfish_data_analysis/master/Code/Common_constants_and_functions.R?token=AH_ZQAXExRItr2tnepmkRr7NRt4hylZrks5aciBtwA%3D%3D", ssl.verifypeer = FALSE)
+# # eval(parse(text = script))
+# script <- getURL("https://raw.githubusercontent.com/pinskylab/Clownfish_data_analysis/master/Code/Common_constants_and_functions.R?token=AH_ZQJT5uCEwjgDGYOneY0W6Zdjol5axks5alHmBwA%3D%3D", ssl.verifypeer = FALSE)
 # eval(parse(text = script))
-script <- getURL("https://raw.githubusercontent.com/pinskylab/Clownfish_data_analysis/master/Code/Common_constants_and_functions.R?token=AH_ZQJT5uCEwjgDGYOneY0W6Zdjol5axks5alHmBwA%3D%3D", ssl.verifypeer = FALSE)
-eval(parse(text = script))
+# 
+# # Functions from Michelle's GitHub helpers script
+# #field_helpers (similar idea to helpers, in field repository) - this might be the newer version of the helpers collection?
+# script <- getURL("https://raw.githubusercontent.com/pinskylab/field/master/scripts/field_helpers.R", ssl.verifypeer = FALSE)
+# eval(parse(text = script))
+# 
+# #################### Running things: ####################
+# ########## Pull info from database
+# leyte <- read_db("Leyte")
+# 
+# allfish_fish <- leyte %>% 
+#   tbl("clownfish") %>%
+#   select(fish_table_id, anem_table_id, fish_spp, sample_id, gen_id, anem_table_id, recap, tag_id, color, size) %>%
+#   collect() %>%
+#   filter(fish_spp == "APCL")
+# 
+# allfish_anems <- leyte %>%
+#   tbl("anemones") %>%
+#   select(anem_table_id, dive_table_id, anem_obs, anem_id, old_anem_id) %>%
+#   collect() %>%
+#   filter(anem_table_id %in% allfish_fish$anem_table_id)
+# 
+# allfish_dives <- leyte %>%
+#   tbl("diveinfo") %>%
+#   select(dive_table_id, dive_type, date, site, gps) %>%
+#   collect() %>%
+#   filter(dive_table_id %in% allfish_anems$dive_table_id)
+# 
+# # pull out just the year and put that in a separate column
+# allfish_dives$year <- as.integer(substring(allfish_dives$date,1,4))
+# allfish_dives$month <- as.integer(substring(allfish_dives$date,6,7))
+# 
+# # join together
+# allfish <- left_join(allfish_fish, allfish_anems, by="anem_table_id")
+# allfish <- left_join(allfish, allfish_dives, by="dive_table_id")
+# 
+# allfish$size <- as.numeric(allfish$size) #make size numeric (rather than a chr) so can do means and such
 
-# Functions from Michelle's GitHub helpers script
-#field_helpers (similar idea to helpers, in field repository) - this might be the newer version of the helpers collection?
-script <- getURL("https://raw.githubusercontent.com/pinskylab/field/master/scripts/field_helpers.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
-
-#################### Running things: ####################
-########## Pull info from database
-leyte <- read_db("Leyte")
-
-allfish_fish <- leyte %>% 
-  tbl("clownfish") %>%
-  select(fish_table_id, anem_table_id, fish_spp, sample_id, cap_id, anem_table_id, recap, tag_id, color, size) %>%
-  collect() %>%
-  filter(fish_spp == "APCL")
-
-allfish_anems <- leyte %>%
-  tbl("anemones") %>%
-  select(anem_table_id, dive_table_id, anem_obs, anem_id, old_anem_id) %>%
-  collect() %>%
-  filter(anem_table_id %in% allfish_fish$anem_table_id)
-
-allfish_dives <- leyte %>%
-  tbl("diveinfo") %>%
-  select(dive_table_id, dive_type, date, site, gps) %>%
-  collect() %>%
-  filter(dive_table_id %in% allfish_anems$dive_table_id)
-
-# pull out just the year and put that in a separate column
-allfish_dives$year <- as.integer(substring(allfish_dives$date,1,4))
-allfish_dives$month <- as.integer(substring(allfish_dives$date,6,7))
-
-# join together
-allfish <- left_join(allfish_fish, allfish_anems, by="anem_table_id")
-allfish <- left_join(allfish, allfish_dives, by="dive_table_id")
-
-allfish$size <- as.numeric(allfish$size) #make size numeric (rather than a chr) so can do means and such
+# #pull out just tagged fish
+# taggedfish <- allfish %>% filter(!is.na(tag_id))
 
 # try to reduce multiple observations of same fish - filter out Jan-Feb 2015 anem surveys
-allfish_abbrev <- allfish %>% filter(month %in% sample_months)
-allfish_abbrev_divetype <- allfish %>% filter(month %in% sample_months) %>% filter(dive_type %in% dive_list)
+allAPCL_abbrev <- allAPCL %>% filter(month %in% sample_months)
+allAPCL_abbrev_divetype <- allAPCL %>% filter(month %in% sample_months) %>% filter(dive_type %in% dive_list)
 
-#pull out just tagged fish
-taggedfish <- allfish %>% filter(!is.na(tag_id))
 
 ########## Estimate number of eggs coming from each site and the whole population each year
 ##### Find number of breeding females
 # Find number of breeding females seen or captured
-breedingF <- allfish %>% 
+breedingF <- allAPCL %>% 
   group_by(site,year) %>%
   filter(color %in% c("Y","YP")) %>% #just filter out YP fish (and Y for 2012, when there was no YP)
   filter(size >= min_size_breedingF) %>% #but also make sure they're big enough to be a breeder (sometimes small fish get marked YP too)
   summarise(NbreedingF = n())
 
 # Find number of breeding females tagged
-breedingF_tagged <- allfish %>%
+breedingF_tagged <- allAPCL %>%
   group_by(year,site) %>%
   filter(color %in% c("Y","YP")) %>%
   filter(!is.na(tag_id)) %>%
-  summarise(Ntagged_breedingF = n())
+  summarise(Ntagged_breedingF = n()) 
 
 # Combine - since didn't tag all years, might not have any females for some site/years
 breedingF_info <- left_join(breedingF, breedingF_tagged, by = c("site" = "site", "year" = "year")) # no 2012 obs here - why?
@@ -191,14 +195,14 @@ breedingF_info <- breedingF_info %>%
 
 ##### Find number of recruits in a pop in each year
 # Find number of recruits (3.5cm - 6.0cm) seen or captured
-recruits <- allfish %>% 
+recruits <- allAPCL %>% 
   group_by(site,year) %>%
   filter(size >= min_size_recruit & size < max_size_recruit) %>% #define "recruits" as fish between 3.5-6.0cm
   filter(dive_type %in% dive_list) %>% #avoid anem and recapture dives to avoid double-counting?
   summarise(Nrecruits = n())
 
 # Find number of recruits clipped - way fewer numbers, what is happening here?
-recruits_clipped <- allfish %>%
+recruits_clipped <- allAPCL %>%
   group_by(year,site) %>%
   filter(size < min_tag_size) %>%
   filter(!is.na(sample_id)) %>%
@@ -238,11 +242,13 @@ recruits_info$egg_year <- recruits_info$year - 1
 #                                       by = c("site" = "site", "year" = "egg_year"))
 # females_recruits_summary <- females_recruits_summary %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)) #since don't have recruits for 2018 female output...
 females_recruits_summary <- left_join((breedingF_info %>% select(-NbreedingF, -Ntagged_breedingF, -prop_hab_sampled_high_TA, -prop_hab_sampled_low_TA, -prop_hab_sampled_metal_TA, -prop_hab_sampled_mid_TA)), 
-                                       (recruits_info %>% select(-prop_hab_sampled_high_TA, -prop_hab_sampled_low_TA, -prop_hab_sampled_metal_TA, -prop_hab_sampled_mid_TA)), by = c("site" = "site", "year" = "egg_year"))
+                                       (recruits_info %>% select(-prop_hab_sampled_high_TA, -prop_hab_sampled_low_TA, -prop_hab_sampled_metal_TA, -prop_hab_sampled_mid_TA, -area_msq)), by = c("site" = "site", "year" = "egg_year"))
+females_recruits_summary <- females_recruits_summary %>%
+  dplyr::rename(egg_year = year) #rename year as "egg year" to make it clear that recruits and females measurements were't taken in the same year
 
 # And one more data frame where each year all females, eggs, and recruits are summed up across sites
 metapop_level <- females_recruits_summary %>%
-  group_by(year) %>% 
+  group_by(egg_year) %>% 
   summarize(metapop_rawF = sum(NbreedingF_combo, na.rm =TRUE), 
             metapop_estF_metalTA = sum(totalF_est_metalTA, na.rm = TRUE),
             metapop_est_eggs_metalTA = sum(est_eggs_metalTA, na.rm = TRUE),
@@ -284,7 +290,7 @@ metapop_level_mod5 <- left_join(metapop_level, meta_egg_recruits_est_predicted_m
 #################### Plots: ####################
 # Number of eggs produced per m2 by site with number of recruits per m2 there a year later, metal tags used for total anem estimates
 pdf(file = here("Plots/EggRecruitRelationship", "Eggs_recruits_by_site_metalTA_m2.pdf"))
-ggplot(data = females_recruits_summary_mod1, aes(x=est_eggs_metalTA_m2, y=totalR_est_metalTA_m2, color=year, shape=site)) +
+ggplot(data = females_recruits_summary_mod1, aes(x=est_eggs_metalTA_m2, y=totalR_est_metalTA_m2, color=egg_year, shape=site)) +
   geom_point(size=3) +
   scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
   geom_line(color = "black", data=females_recruits_summary_mod1, aes(x=est_eggs_metalTA_m2, y=recruits_pred_mod1)) +
@@ -297,7 +303,7 @@ dev.off()
 
 # Number of eggs produced per m2 by site with number of recruits per m2 there a year later, mean total anems used for total anem estimates
 pdf(file = here("Plots/EggRecruitRelationship", "Eggs_recruits_by_site_midTA_m2.pdf"))
-ggplot(data = females_recruits_summary_mod2, aes(x=est_eggs_midTA_m2, y=totalR_est_midTA_m2, color=year, shape=site)) +
+ggplot(data = females_recruits_summary_mod2, aes(x=est_eggs_midTA_m2, y=totalR_est_midTA_m2, color=egg_year, shape=site)) +
   geom_point(size=3) +
   scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
   geom_line(color = "black", data=females_recruits_summary_mod2, aes(x=est_eggs_midTA_m2, y=recruits_pred_mod2)) +
@@ -310,7 +316,7 @@ dev.off()
 
 # Number of eggs produced by site with number of recruits there a year later, at site level - updated to be w/estimates using metal tags as TA
 pdf(file = here("Plots/EggRecruitRelationship", "Eggs_recruits_by_site.pdf"))
-ggplot(data = females_recruits_summary_mod1, aes(x=est_eggs_metalTA, y=totalR_est_metalTA, color=year, shape=site)) +
+ggplot(data = females_recruits_summary_mod1, aes(x=est_eggs_metalTA, y=totalR_est_metalTA, color=egg_year, shape=site)) +
   geom_point(size=3) +
   scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
   #geom_line(color = "black", data=females_recruits_summary_mod1, aes(x=est_eggs_metalTA, y=recruits_pred_mod1)) +
@@ -323,7 +329,7 @@ dev.off()
 
 # Number of eggs produced by site with number of recruits there a year later, at site level, ESA presentation
 pdf(file = here("Plots/EggRecruitRelationship", "Eggs_recruits_by_site_ESA.pdf"))
-ggplot(data = females_recruits_summary_mod1, aes(x=est_eggs, y=totalR_est, color=year)) +
+ggplot(data = females_recruits_summary_mod1, aes(x=est_eggs, y=totalR_est, color=egg_year)) +
   geom_point(size=4) +
   #scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
   geom_line(color = "black", data=females_recruits_summary_mod1, aes(x=est_eggs, y=recruits_pred)) +
@@ -341,7 +347,7 @@ dev.off()
 
 # Number of eggs produced by site with number of recruits there a year later, at site level, same as above but for MPE poster
 pdf(file = here("Plots/EggRecruitRelationship", "Eggs_recruits_by_site_MPEposter.pdf"))
-ggplot(data = females_recruits_summary, aes(x=est_eggs, y=totalR_est, color=year)) +
+ggplot(data = females_recruits_summary, aes(x=est_eggs, y=totalR_est, color=egg_year)) +
   geom_point(size=4) +
   xlab("egg output") + ylab("recruits") +
   #ggtitle("Egg-recruits") +
@@ -353,7 +359,7 @@ dev.off()
 
 # Number of eggs produced by site with number of recruits there a year later, years broken out individually - updated to use estimates from metal tags as TA
 pdf(file = here("Plots/EggRecruitRelationship", "Eggs_recruits_by_site_years_as_subplots.pdf"))
-ggplot(data = females_recruits_summary %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=est_eggs_metalTA, y=totalR_est_metalTA, shape=site)) +
+ggplot(data = females_recruits_summary %>% filter(egg_year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=est_eggs_metalTA, y=totalR_est_metalTA, shape=site)) +
   geom_point(size=3) +
   scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
   xlab("estimated egg output") + ylab("estimated number of recruits following year") +
@@ -368,7 +374,7 @@ dev.off()
 
 # Same as above but w/estimated number of females instead of eggs (which looks exactly the same right now b/c eggs aren't dependent on female size yet) - updated to use estimates from metal tags as TA
 pdf(file = here("Plots/EggRecruitRelationship", "Females_recruits_by_site.pdf"))
-ggplot(data = females_recruits_summary %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=totalF_est_metalTA, y=totalR_est_metalTA, color=year, shape=site)) +
+ggplot(data = females_recruits_summary %>% filter(egg_year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=totalF_est_metalTA, y=totalR_est_metalTA, color=year, shape=site)) +
   geom_point(size=3) +
   scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
   scale_fill_brewer(palette = "Set1") + #not sure why this is having no effect...
@@ -381,7 +387,7 @@ dev.off()
 
 # Truncated w/estimated number of females instead of eggs (which looks exactly the same right now b/c eggs aren't dependent on female size yet) - updated to use metal tags TA estimates
 pdf(file = here("Plots/EggRecruitRelationship", "Females_recruits_by_site_truncated.pdf"))
-ggplot(data = females_recruits_summary %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=totalF_est_metalTA, y=totalR_est_metalTA, color=year, shape=site)) +
+ggplot(data = females_recruits_summary %>% filter(egg_year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=totalF_est_metalTA, y=totalR_est_metalTA, color=year, shape=site)) +
   geom_point(size=3) +
   scale_x_continuous(limits=c(0,150)) +
   scale_shape_manual(values = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)) +
@@ -408,7 +414,7 @@ dev.off()
 
 # Now sum it up by year across all sites, plot females and recruits at metapop level - updated to use estimates that use metal TA
 pdf(file = here("Plots/EggRecruitRelationship", "Metapop_females_recruits_estimated.pdf"))
-ggplot(data = metapop_level %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=metapop_estF_metalTA, y=metapop_estR_metalTA, color=year)) +
+ggplot(data = metapop_level %>% filter(egg_year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=metapop_estF_metalTA, y=metapop_estR_metalTA, color=year)) +
   geom_point(size=3) +
   xlab("estimated # F") + ylab("estimated # R following year") +
   ggtitle("Estimated F and R summed across all sites sampled in each year (metal TA)") +
@@ -419,7 +425,7 @@ dev.off()
 
 # Now sum it up by year across all sites, plot est eggs and recruits at metapop level - updated to use estimates from metal TA
 pdf(file = here("Plots/EggRecruitRelationship", "Metapop_eggs_recruits_estimated.pdf"))
-ggplot(data = metapop_level_mod5 %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=metapop_est_eggs_metalTA, y=metapop_estR_metalTA, color=year)) +
+ggplot(data = metapop_level_mod5 %>% filter(egg_year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=metapop_est_eggs_metalTA, y=metapop_estR_metalTA, color=year)) +
   geom_point(size=3) +
   geom_line(color = "black", data=metapop_level_mod5, aes(x=metapop_est_eggs_metalTA, y=recruits_pred_mod5)) +
   xlab("estimated # eggs") + ylab("estimated # R following year") +
@@ -431,7 +437,7 @@ dev.off()
 
 # Now sum it up by year across all sites, plot raw females and recruits at metapop level
 pdf(file = here("Plots/EggRecruitRelationship", "Metapop_females_recruits_raw.pdf"))
-ggplot(data = metapop_level %>% filter(year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=metapop_rawF, y=metapop_rawR, color=year)) +
+ggplot(data = metapop_level %>% filter(egg_year %in% c(2012,2013,2014,2015,2016,2017)), aes(x=metapop_rawF, y=metapop_rawR, color=year)) +
   geom_point(size=3) +
   xlab("raw # F") + ylab("raw # R following year") +
   ggtitle("Raw F and R summed across all sites sampled in each year") +
@@ -442,14 +448,15 @@ dev.off()
 
 #################### Saving output: ####################
 save(site_areas, file=here::here('Data','site_areas.RData'))
-save(recruits_info, file=here("Data", "recruits_info.RData"))
-save(females_recruits_summary, file=here("Data", "females_recruits_summary.RData"))
-save(metapop_level, file=here("Data", "metapop_level.RData"))
-save(egg_recruits_est_metalTA_m2_mod1, file=here("Data","egg_recruit_lm_mod1.RData"))
-save(egg_recruits_est_midTA_m2_mod2, file=here("Data", "egg_recruit_lm_mod2.RData"))
-save(females_recruits_est_metalTA_m2_mod3, file=here("Data", "female_recruit_lm_mod3.RData"))
-save(females_recruits_est_midTA_m2_mod4, file=here("Data", "female_recruit_lm_mod4.RData"))
-save(meta_eggs_recruits_est_mod5, file=here("Data","metapop_lm_mod5.RData"))
+save(recruits_info, file=here::here("Data", "recruits_info.RData"))
+save(breedingF_info, file=here::here('Data', 'breedingF_info.RData'))
+save(females_recruits_summary, file=here::here("Data", "females_recruits_summary.RData"))
+save(metapop_level, file=here::here("Data", "metapop_level.RData"))
+save(egg_recruits_est_metalTA_m2_mod1, file=here::here("Data","egg_recruit_lm_mod1.RData"))
+save(egg_recruits_est_midTA_m2_mod2, file=here::here("Data", "egg_recruit_lm_mod2.RData"))
+save(females_recruits_est_metalTA_m2_mod3, file=here::here("Data", "female_recruit_lm_mod3.RData"))
+save(females_recruits_est_midTA_m2_mod4, file=here::here("Data", "female_recruit_lm_mod4.RData"))
+save(meta_eggs_recruits_est_mod5, file=here::here("Data","metapop_lm_mod5.RData"))
 
 
 #Thinking through the plan
