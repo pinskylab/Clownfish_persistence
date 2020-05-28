28e8eb8e0e7302971067d203503f6f51c314231b
Allison Dedrick <agdedrick@ucdavis.edu>
2020-05-28 10:11:40 -0700

    Deleted extraneous comments from Pull data script

--- Code/Pull_data_from_database.R
+++ Code/Pull_data_from_database.R
@@ -7,9 +7,6 @@ library(dplyr)
 library(here)
 
 #################### Functions: ####################
-# # Functions from Michelle's GitHub helpers script so can connect to database
-# script <- getURL("https://raw.githubusercontent.com/pinskylab/field/master/scripts/field_helpers.R", ssl.verifypeer = FALSE)
-# eval(parse(text = script))
 
 # Function with myconfig file info in it, for some reason new version of R/RStudio can't find the database...
 read_db <- function(x) {
@@ -34,10 +31,6 @@ gps_db <- leyte %>% tbl("GPX") %>% collect()  # gps info table
 
 #################### Pull data from GitHub from other analyses: ####################
 
-# # Fish obs table linking together individual fish marked and recaught by both genetic and tag methods
-# download.file(url = "https://github.com/pinskylab/genomics/blob/master/data/fish-obs.RData?raw=true", destfile = "fish-obs.RData")
-# fish_obs <- readRDS("fish-obs.RData")
-
 # Fish obs table linking together individual fish marked and recaught by both genetic and tag methods
 fish_obs <- read.csv(text = getURL("https://raw.githubusercontent.com/pinskylab/genomics/master/data/fish-obs.csv"), header = TRUE, stringsAsFactors = FALSE)
 
@@ -45,15 +38,14 @@ fish_obs <- read.csv(text = getURL("https://raw.githubusercontent.com/pinskylab/
 site_centers <- read.csv(file = here::here("Data/From_other_analyses", "site_centroids.csv"), header = TRUE, stringsAsFactors = FALSE)
 
 # Kernel fits
-kernel_summary <- read.csv(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/kernel_fitting/1340_loci/final_results/tables/kernel_fitting_summary.csv"), header = T, stringsAsFactors = F)
-k_theta_allyear_95CI_values <- read.csv(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/kernel_fitting/1340_loci/final_results/likelihood_profiles_grid_search/profile95CI_AllYears.csv"), header = T, stringsAsFactors = F)
-#k_theta_allyear_95CI_values <- read.csv(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/kernel_fitting/1340_loci/final_results/likelihood_profiles_grid_search/AllYearParams95CI.csv"), header = T, stringsAsFactors = F)
+kernel_summary <- read.csv(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/kernel_fitting/1340_loci/final_results/tables/kernel_fitting_summary.csv"), header = TRUE, stringsAsFactors = FALSE)
+k_theta_allyear_95CI_values <- read.csv(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/kernel_fitting/1340_loci/final_results/likelihood_profiles_grid_search/profile95CI_AllYears.csv"), header = TRUE, stringsAsFactors = FALSE)
 
 # List of all parents put into parentage analysis (so can match to site)
-all_parents <- read.table(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/colony2/20190523_1340loci/input/all_parents_corrected.txt"), header = T, stringsAsFactors = F)
+all_parents <- read.table(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/colony2/20190523_1340loci/input/all_parents_corrected.txt"), header = TRUE, stringsAsFactors = FALSE)
 
 # List of all offspring put into parentage analysis
-all_offspring <- read.table(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/colony2/20190523_1340loci/input/all_offspring_corrected.txt"), header = T, stringsAsFactors = F)
+all_offspring <- read.table(text = getURL("https://raw.githubusercontent.com/katcatalano/parentage/master/colony2/20190523_1340loci/input/all_offspring_corrected.txt"), header = TRUE, stringsAsFactors = FALSE)
 
 # Size-fecundity model from Adam
 load(file=here::here('Data', 'loglogFecunditySizeModel.RData'))  # size-fecundity output for best-fit model from Adam, called length_count8llEA
@@ -75,5 +67,4 @@ save(all_offspring, file = here::here("Data/From_other_analyses", "all_offspring
 save(k_theta_allyear_95CI_values, file = here::here("Data/From_other_analyses", "k_theta_allyear_95CI_values.RData"))  # 95% CI values for k and theta fits
 save(length_count8llEA, file= here::here("Data/From_other_analyses", "length_count8llEA.RData"))  # size-fecundity model from Adam
 
-# saveRDS() - use this so that people can load things and choose their own names rather than being stuck with the names I choose
 
